6c056dcd2fc8db4dd5f0b5ca6041394763c6ac2d
Raphael Ribeiro <63366513+Freinschen@users.noreply.github.com>
2021-01-26 20:57:20 -0300

    Add files via upload

---
+++ DSA Academy.py
@@ -0,0 +1,10 @@
+# DSA Academy
+from math import sqrt
+tipo = type(5)
+arredondado = round(5.84711)
+num = pow(5,2)
+raiz = sqrt(10)
+print("Agora eu posso programar em python")
+print(num)
+print(arredondado)
+print(f"{raiz:.2f}" )
